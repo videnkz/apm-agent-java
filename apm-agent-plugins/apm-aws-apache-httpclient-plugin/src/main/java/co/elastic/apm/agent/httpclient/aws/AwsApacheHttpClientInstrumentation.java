6ff1928b726e70723e955cea97b0d1e8cf1e90e8
kananindzya <kananindzya@gmail.com>
2019-06-07 18:22:39 +0600

    added sync instrumentation for aws apache httpclient

---
+++ apm-agent-plugins/apm-aws-apache-httpclient-plugin/src/main/java/co/elastic/apm/agent/httpclient/aws/AwsApacheHttpClientInstrumentation.java
@@ -0,0 +1,114 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 - 2019 Elastic and contributors
+ * %%
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * #L%
+ */
+package co.elastic.apm.agent.httpclient.aws;
+
+import co.elastic.apm.agent.bci.ElasticApmInstrumentation;
+import co.elastic.apm.agent.http.client.HttpClientHelper;
+import co.elastic.apm.agent.impl.transaction.Span;
+import co.elastic.apm.agent.impl.transaction.TraceContext;
+import co.elastic.apm.agent.impl.transaction.TraceContextHolder;
+import com.amazonaws.thirdparty.apache.http.client.methods.CloseableHttpResponse;
+import com.amazonaws.thirdparty.apache.http.client.methods.HttpRequestWrapper;
+import com.amazonaws.thirdparty.apache.http.conn.routing.HttpRoute;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.NamedElement;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static net.bytebuddy.matcher.ElementMatchers.hasSuperType;
+import static net.bytebuddy.matcher.ElementMatchers.nameContains;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.returns;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+public class AwsApacheHttpClientInstrumentation extends ElasticApmInstrumentation {
+
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    private static void onBeforeExecute(@Advice.Argument(0) HttpRoute route,
+                                        @Advice.Argument(1) HttpRequestWrapper request,
+                                        @Advice.Local("span") Span span) {
+        if (tracer == null || tracer.getActive() == null) {
+            return;
+        }
+        final TraceContextHolder<?> parent = tracer.getActive();
+        span = HttpClientHelper.startHttpClientSpan(parent, request.getMethod(), request.getURI(), route.getTargetHost().getHostName());
+        if (span != null) {
+            span.activate();
+            request.addHeader(TraceContext.TRACE_PARENT_HEADER, span.getTraceContext().getOutgoingTraceParentHeader().toString());
+        } else if (!request.containsHeader(TraceContext.TRACE_PARENT_HEADER) && parent != null) {
+            // re-adds the header on redirects
+            request.addHeader(TraceContext.TRACE_PARENT_HEADER, parent.getTraceContext().getOutgoingTraceParentHeader().toString());
+        }
+    }
+
+    @Advice.OnMethodExit(suppress = Throwable.class, onThrowable = Throwable.class)
+    public static void onAfterExecute(@Advice.Return @Nullable CloseableHttpResponse response,
+                                      @Advice.Local("span") @Nullable Span span,
+                                      @Advice.Thrown @Nullable Throwable t) {
+        if (span != null) {
+            try {
+                if (response != null && response.getStatusLine() != null) {
+                    int statusCode = response.getStatusLine().getStatusCode();
+                    span.getContext().getHttp().withStatusCode(statusCode);
+                }
+                span.captureException(t);
+            } finally {
+                span.deactivate().end();
+            }
+        }
+    }
+
+    @Override
+    public ElementMatcher<? super NamedElement> getTypeMatcherPreFilter() {
+        return nameContains("Exec").or(nameContains("Chain"));
+    }
+
+    @Override
+    public ElementMatcher<? super TypeDescription> getTypeMatcher() {
+        return hasSuperType(named("com.amazonaws.thirdparty.apache.http.impl.execchain.ClientExecChain"));
+    }
+
+    @Override
+    public ElementMatcher<? super MethodDescription> getMethodMatcher() {
+        return named("execute")
+            .and(takesArguments(4))
+            .and(returns(hasSuperType(named("com.amazonaws.thirdparty.apache.http.client.methods.CloseableHttpResponse"))))
+            .and(takesArgument(0, hasSuperType(named("com.amazonaws.thirdparty.apache.http.conn.routing.HttpRoute"))))
+            .and(takesArgument(1, hasSuperType(named("com.amazonaws.thirdparty.apache.http.client.methods.HttpRequestWrapper"))))
+            .and(takesArgument(2, hasSuperType(named("com.amazonaws.thirdparty.apache.http.client.protocol.HttpClientContext"))))
+            .and(takesArgument(3, hasSuperType(named("com.amazonaws.thirdparty.apache.http.client.methods.HttpExecutionAware"))));
+    }
+
+    @Override
+    public Collection<String> getInstrumentationGroupNames() {
+        return Arrays.asList("http-client", "aws-apache-httpclient");
+    }
+}
---
+++ apm-agent-plugins/apm-aws-apache-httpclient-plugin/src/test/java/co/elastic/apm/agent/httpclient/aws/ApacheHttpClientInstrumentationTest.java
@@ -0,0 +1,48 @@
+/*-
+ * #%L
+ * Elastic APM Java agent
+ * %%
+ * Copyright (C) 2018 - 2019 Elastic and contributors
+ * %%
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * #L%
+ */
+package co.elastic.apm.agent.httpclient.aws;
+
+import co.elastic.apm.agent.httpclient.AbstractHttpClientInstrumentationTest;
+
+import com.amazonaws.thirdparty.apache.http.client.HttpClient;
+import com.amazonaws.thirdparty.apache.http.client.methods.HttpGet;
+import com.amazonaws.thirdparty.apache.http.impl.client.HttpClients;
+import org.junit.Before;
+
+public class ApacheHttpClientInstrumentationTest extends AbstractHttpClientInstrumentationTest {
+
+    private HttpClient client;
+
+    @Before
+    public void setUp() {
+        client = HttpClients.createDefault();
+    }
+
+    @Override
+    protected void performGet(String path) throws Exception {
+        client.execute(new HttpGet(path)).getStatusLine().getStatusCode();
+    }
+
+}
